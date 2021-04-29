05de72fd4acdff1f0cbf2bbd4c1ccc82af3599e0
Jordan Adams <jordan@jordanplayz158.me>
2021-04-29 14:43:34 -0400

    MemberVcListener.java - Cleaned up code - Fixed errors with code (didn't actually remove roles from discord api)

--- src/main/java/me/jordanplayz158/phasestaff/events/MemberVcListener.java
+++ src/main/java/me/jordanplayz158/phasestaff/events/MemberVcListener.java
@@ -2,6 +2,7 @@ package me.jordanplayz158.phasestaff.events;
 
 import me.jordanplayz158.phasestaff.PhaseStaff;
 import me.jordanplayz158.phasestaff.json.Config;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
@@ -10,32 +11,31 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 public class MemberVcListener extends ListenerAdapter {
+    private Config config = PhaseStaff.getInstance().getConfig();
 
     @Override
-    public void onGuildVoiceJoin(GuildVoiceJoinEvent event) {
-        System.out.println("Worked :)");
-        super.onGuildVoiceJoin(event);
+    public void onGuildVoiceJoin(@NotNull GuildVoiceJoinEvent event) {
+        PhaseStaff.logger.debug("Entered Listener - join");
+
+        Guild guild = event.getGuild();
         Member member = event.getMember();
-        Long id = PhaseStaff.getInstance().getConfig().getVcRoleId();
-        Role role = member.getGuild().getRoleById(id);
+        Role role = guild.getRoleById(config.getVcRoleId());
         if (!member.getRoles().contains(role)) {
-            member.getRoles().add(role);
-
+            assert role != null;
+            guild.addRoleToMember(member.getId(), role).queue();
         }
-
     }
 
     @Override
     public void onGuildVoiceLeave(@NotNull GuildVoiceLeaveEvent event) {
-        System.out.println("Worked :-)");
+        PhaseStaff.logger.debug("Entered Listener - leave");
 
-        super.onGuildVoiceLeave(event);
+        Guild guild = event.getGuild();
         Member member = event.getMember();
-        Long id = PhaseStaff.getInstance().getConfig().getVcRoleId();
-        Role role = member.getGuild().getRoleById(id);
-        if (member.getRoles().contains(role)) {
-            member.getRoles().remove(role);
-
+        Role role = guild.getRoleById(config.getVcRoleId());
+        if(member.getRoles().contains(role)) {
+            assert role != null;
+            guild.removeRoleFromMember(member.getId(), role).queue();
         }
     }
 }
